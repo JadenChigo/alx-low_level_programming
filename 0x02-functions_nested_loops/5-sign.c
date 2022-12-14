49e8dc1e6197162abfe047b1c54699e521ca4071
Emmanuel Chigozirim <emmanuelchigozirim@yahoo.com>
2022-12-14 22:20:45 +0100

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *  print_sign - prints the sign of a number
+ *  @n: the number that is to be checked
+ *  Return: 1, 0, -1 depending on the final result
+ */
+int print_sign(int n)
+{
+	int v;
+
+	if (n > 0)
+	{
+		v = 1;
+		_putchar('+');
+	}
+	else if (n = 0)
+	{
+		v = 0;
+		_putchar('0');
+	}
+	else
+	{
+		v = -1;
+		_putchar('-');
+	}
+	return (v);
+}
