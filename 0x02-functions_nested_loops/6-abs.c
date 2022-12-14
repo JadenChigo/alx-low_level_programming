7df3b656e15b51d904e137e02a6e136715de8f48
Emmanuel Chigozirim <emmanuelchigozirim@yahoo.com>
2022-12-14 22:37:22 +0100

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  _abs - prints the absolute value of an integer
+ *  @m: the int to be used
+ *
+ *  Return: an unsigned int
+ */
+int _abs(int)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		n *= -1;
+		return (n);
+	}
+}
