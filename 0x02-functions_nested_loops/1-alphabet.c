f75b86bc4ef25742a0e2de8a284275b36b40c8d8
Emmanuel Chigozirim <emmanuelchigozirim@yahoo.com>
2022-12-14 03:59:39 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  main - prints alphabets in lowercase, followed by a newline
+ *
+ *  Return: Always 0 (Success)
+ */
+
+void print_alphabet(void);
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	return (0);
+}
