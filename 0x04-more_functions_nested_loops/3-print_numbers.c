3494e57a811c8675110b10afbe3029a19a88727c
Emmanuel Chigozirim <emmanuelchigozirim@yahoo.com>
2022-12-16 22:57:28 +0100

    more printing

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,11 +4,12 @@
  * print_numbers - prints numbers from 0 to 9
  * Return: Always 0
  */
+
 void print_numbers(void)
 {
 	char b;
 
-	for (b = 48; b <= 57; b++)
+	for (b = '0'; b <= '9'; b++)
 	{
 		_putchar(b);
 	}
