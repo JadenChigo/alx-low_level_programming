c09780698b2af25f2699e493ac6a5d20fc54337b
Emmanuel Chigozirim <emmanuelchigozirim@yahoo.com>
2022-12-16 23:20:25 +0100

    more printing

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,18 @@
 
 void print_most_numbers(void)
 {
-	int a;
+	char a;
 
-	for (a = 48; a <= 57; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		if (a != 50 && a != 52)
+		if (a != '2')
 		{
+			if (a != '4')
+			{
+				_putchar(a);
+			}
 		}
-		_putchar(a);
+
 	}
 		_putchar('\n');
 }
