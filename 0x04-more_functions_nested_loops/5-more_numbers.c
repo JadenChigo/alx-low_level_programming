64f58164680136c8d5569b69791fa08f209ff72b
Emmanuel Chigozirim <emmanuelchigozirim@yahoo.com>
2022-12-16 23:33:02 +0100

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers (1 to 14)
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	char m;
+
+	for (m = '0'; m < '15'; m++)
+	{
+		while (m < 10)
+		{
+			_putchar('\n');
+			m++;
+		}
+		_putchar(m);
+	}
+	_putchar('\n');
+}
+
+
