5752416f3d010e7476406f7c15c70a904b91a63f
Emmanuel Chigozirim <emmanuelchigozirim@yahoo.com>
2022-12-20 20:05:14 +0100

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: the string 
+ */
+void print_rev(char *s)
+{
+	int b = 0;
+
+	while (s[b] != '\0')
+	{
+		b++;
+	}
+	for (b -= 1; b >= 0; b--)
+	{
+		_putchar(s[b]);
+	}
+		_putchar('\n');
+}
