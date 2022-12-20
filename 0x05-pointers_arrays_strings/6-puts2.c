441bc9e5767f26afbfe1224dd69a66c4fd9b7d80
Emmanuel Chigozirim <emmanuelchigozirim@yahoo.com>
2022-12-20 21:47:27 +0100

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string starting with the 
+ * puts2 - prints every other character of a string starting with the
  * first character followed by a newline
- * @s: the string
+ * @str: the string
  */
 void puts2(char *str)
 {
