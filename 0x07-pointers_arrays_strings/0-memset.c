c7726eb8a435f3aac23317a03e654b58637460bb
Emmanuel Chigozirim <emmanuelchigozirim@yahoo.com>
2023-01-03 21:33:27 +0100

    MEL

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,9 +2,9 @@
 
 /**
  * *_memset - fills the memory with constant byte
- * s: memory area
- * n: the size of memory area
- * b: the content of memory area
+ * @s: memory area
+ * @n: the size of memory area
+ * @b: the content of memory area
  * Return: s
  */
 char *_memset(char *s, char b, unsigned int n)
@@ -15,5 +15,5 @@ char *_memset(char *s, char b, unsigned int n)
 	{
 		s[i] = (char)b;
 	}
-	return(s);
+	return (s);
 }
