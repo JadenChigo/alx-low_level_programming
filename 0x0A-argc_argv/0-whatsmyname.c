faf3f4e69e872125d6fe0294d5d3fcaf4eae7ae8
Emmanuel Chigozirim <emmanuelchigozirim@yahoo.com>
2023-01-09 23:49:58 +0100

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,15 +3,18 @@
 
 /**
  * main - Entry point
+ * @argc: numbeer of arguments passed
+ * @argv: strings passed
  * Return: Always 0
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
-	return 0;
+	return (0);
 }
