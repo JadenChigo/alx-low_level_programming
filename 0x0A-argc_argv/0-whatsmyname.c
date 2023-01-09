a647af72d93d00838596a75e8760147a3e664e63
Emmanuel Chigozirim <emmanuelchigozirim@yahoo.com>
2023-01-09 23:34:18 +0100

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(int argc, char* argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return 0;
+}
